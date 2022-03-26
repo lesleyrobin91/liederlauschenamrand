d3221dcdf063b6bdcd115d22470033c985f338b8
lesleyrobin91 <85639756+lesleyrobin91@users.noreply.github.com>
2022-03-26 17:32:19 +0100

    image plant

--- scripts.js
+++ scripts.js
@@ -2,3 +2,14 @@ $('.hamburger-button').click(function(){
     $('.mobile-menu').fadeToggle(100);
     $(this).toggleClass('active');
 });
+
+// initializing the pop-up gallery
+lightGallery(document.getElementById('lightgallery'), {
+    download: false,
+    speed: 500
+  });
+  
+  lightGallery(document.getElementById('lightgallery-2'), {
+    download: false,
+    speed: 500
+  });
